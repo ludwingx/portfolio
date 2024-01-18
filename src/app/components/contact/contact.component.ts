b17b724b47863de29335099b19aa4e71e4dafc58
ludwingx <ludwingytx@gmail.com>
2024-01-18 19:10:51 -0400

    0.1.9

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import * as emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -8,38 +8,39 @@ import * as emailjs from 'emailjs-com';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+
+  /* validaciones de los campos */
   name =new FormControl ('', Validators.required);
   email =new FormControl ('', [Validators.email,Validators.required]);
+  message = new FormControl ('', Validators.required);
+  form: FormGroup = this.fb.group({
+    name: '',
+    email: '',
+    message: ''
+  });
+  constructor(private fb: FormBuilder) { }
+    /* enviar email */
+  async sendEmail() {
+    emailjs.init('LYwK9tZhKYQL5jUnF'); 
+    let response = await emailjs.send("service_b5t6tmp","template_zireym6",{ 
+      name: this.form.value.name, 
+      email: this.form.value.email,
+      message: this.form.value.message
+
+      }).then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+     }, (err) => {
+        console.log('FAILED...', err);
+     });
+  }
+  /* cada 2 segundos se cambia el texto de email  */
   emailTexts = ["ludwingarmijosaavedra@hotmail.com", "ludwingarmijosaavedra@gmail.com"];
   currentEmailText = this.emailTexts[0];
-  form: FormGroup = this.fb.group({
-    user_name: '',
-    user_email: '',
-    user_message: ''
-  });
-
-  constructor(private fb: FormBuilder) { }
-
-  sendEmail() {
-    emailjs.send('service_b5t6tmp', 'template_zireym6', {
-      user_name: this.form.value.user_name,
-      user_email: this.form.value.user_email,
-      user_message: this.form.value.user_message
-    }, 'LYwK9tZhKYQL5jUnF')
-      .then((response) => {
-        console.log('Correo electrónico enviado:', response);
-      })
-      .catch((error) => {
-        console.error('Error al enviar el correo electrónico:', error);
-      });
-  }
-
   ngOnInit(): void {
     setInterval(() => {
       this.changeEmailText();
     }, 2000);
   }
-
   changeEmailText() {
     const currentIndex = (this.emailTexts.indexOf(this.currentEmailText) + 1) % this.emailTexts.length;
     this.currentEmailText = this.emailTexts[currentIndex];
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -15,7 +15,41 @@ export class PortfolioComponent {
       title:'AppCea',
       description:'Aplicación Movil Web desarrollada con Ionic, Angular, PHP y MySQL para el Centro de Educación Ambiental del Gobierno'+ 
       ' Autónomo Departamental de Santa Cruz',
+    },
+    { 
+      imageUrl:'./assets/pp.jpg',
+      title:'EasyBank',
+      description:'#RetoEasyBank de programación de una landing page, creada con Angular17 y Bootstrap5',
+    },
+    { 
+      imageUrl:'./assets/pp.jpg',
+      title:'News Homepage',
+      description:'#RetoNewsHomepage de programación de una landing page, creada con Angular17 y Bootstrap5',
+    },
+    { 
+      imageUrl:'./assets/pp.jpg',
+      title:'Alley Plants',
+      description:'Landing page, creada con Angular16 y Bootstrap5',
+    },
+    { 
+      imageUrl:'./assets/pp.jpg',
+      title:'Portafolio',
+      description:'LadingPage creada con Angular16 y Bootstrap5, con el fin de mostrar mis proyectos realizados',
+    }
+  ]
+  formatDescription(description: string): string {
+    // Definir múltiples patrones y reemplazos
+    const replacements = [
+      { pattern: /#RetoEasyBank/g, link: 'https://www.frontendmentor.io/challenges/easybank-landing-page-WaUhkoDN' },
+      { pattern: /#RetoNewsHomepage/g, link: 'https://www.frontendmentor.io/challenges/news-homepage-4uTIT6jCd' },
+      // Puedes agregar más patrones y enlaces aquí según sea necesario
+    ];
+
+    // Aplicar los reemplazos en la descripción
+    for (const replacement of replacements) {
+      description = description.replace(replacement.pattern, `<a target="_blank" href="${replacement.link}">#Reto</a>`);
     }
 
-  ]
+    return description;
+  }
 }
