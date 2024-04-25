16dc74f1c7d972d0525e7eeaeda8840264c94275
ludwingx <ludwingytx@gmail.com>
2024-04-24 21:59:48 -0400

    portfolio v2.0

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
   txteyebutton = "View Site"
-  txtgithubbutton = "Code"
+  txtgithubbutton = "Code FullStack"
+  txtgithubbutton2 = "Code Frontend"
+  txtgithubbutton3 = "Code Backend"
 
   cards = [
     { 
@@ -15,12 +17,16 @@ export class PortfolioComponent {
       title:'Farmacia Santa Rosita APP',
       description:'Aplicación Web desarrollada para la gestión de inventario y ventas de la Farmacia Santa Rosita, utilizando Angular para el frontend y NodeJs con MySQL para el backend.',
       technologies: ['Angular17', 'HTML', 'TypeScript', 'NodeJs', 'SCSS', 'MySQL', 'Bootstrap5'],
+      codeUrl2: 'https://github.com/ludwingx/farmacia-santa-rosita-app-frontend',
+      codeUrl3: 'https://github.com/ludwingx/farmacia-santa-rosita-app-backend' 
     },
     { 
       imageUrl:'./assets/amorConPatas.png',
       title:'Amor Con Patas APP',
       description:'Aplicación Web desarrollada con Laravel, PHP, MySQL y Bootstrap5, para dar en adopción animales de diferentes refugios de la Ciudad de Santa Cruz.',
       technologies: ['Laravel', 'PHP','JavaScript','AJAX', 'MySQL', 'Bootstrap5'],
+      codeUrl: 'https://github.com/ludwingx/amor-con-patas-app'
+      
     },
     { 
       imageUrl:'./assets/appCEA.png',
@@ -28,36 +34,43 @@ export class PortfolioComponent {
       description:'Aplicación Movil Web desarrollada con Ionic, Angular, PHP y MySQL para el Control y seguimiento de animales silvestres rescatados del Centro de Educación Ambiental del Gobierno '+ 
       'Autónomo Departamental de Santa Cruz',
       technologies: ['Angular17','HTML', 'TypeScript',  'SCSS', 'Ionic','PHP',, 'MySQL', 'Bootstrap5'],
+      codeUrl: 'https://github.com/ludwingx/AppCEA' 
     },
     { 
       imageUrl:'./assets/easybank.png',
       title:'EasyBank',
       description:'#RetoEasyBank de programación de una landing page, creada con Angular17 y Bootstrap5',
       technologies: ['Angular17', 'HTML','TypeScript', 'SCSS', 'Bootstrap5'],
+      codeUrl2: 'https://github.com/ludwingx/Easybank-Challange'
     },
     { 
       imageUrl:'./assets/newHome.png',
       title:'News Homepage',
       description:'#RetoNewsHomepage de programación de una landing page, creada con Angular17 y Bootstrap5',
       technologies: ['Angular17','HTML', 'Typescript', 'SCSS', 'Bootstrap5'],
+      codeUrl2: 'https://github.com/ludwingx/news-homepage'
     },
     { 
       imageUrl:'./assets/multiStepForm.png',
       title:'MultiStepForm',
       description:'#RetoMultiStepForm de programación de una landing page, creada con Angular17 y Bootstrap5',
       technologies: ['Angular17', 'HTML','Typescript', 'SCSS', 'Bootstrap5'],
+      codeUrl2: 'https://github.com/ludwingx/multi-step-form'
     },
     { 
       imageUrl:'./assets/alleyPlants.png',
       title:'Alley Plants',
-      description:'Landing page, creada con Angular16 y Bootstrap5',
+      description:'Landing page, creada con Angular16 y Bootstrap5 para vivero de suculentas en Santa Cruz',
       technologies: ['Angular16','HTML', 'Typescript', 'NodeJs', 'SCSS', 'MySQL', 'Bootstrap5'],
+      codeUrl2: 'https://github.com/ludwingx/alley-plants'
     },
     { 
       imageUrl:'./assets/portfolio.png',
       title:'Portafolio',
       description:'LadingPage creada con Angular16 y Bootstrap5, con el fin de mostrar mis proyectos realizados',
       technologies: ['Angular16', 'HTML','Typescript', 'SCSS', 'Bootstrap5'],
+      siteUrl: 'https://6629a8630acff635f7857d53--symphonious-lily-3451a2.netlify.app/',
+      codeUrl2: 'https://github.com/ludwingx/my-portfolio'
     }
   ]
   formatDescription(description: string): string {
@@ -79,5 +92,27 @@ export class PortfolioComponent {
 
     return description;
   }
+  viewSite(card: any) {
+    if (card.siteUrl) {
+      window.open(card.siteUrl, '_blank');
+    }
+  }
+
+  viewCode(card: any) {
+    if (card.codeUrl) {
+      window.open(card.codeUrl, '_blank');
+    }
+  }
+  viewCode2(card: any) {
+    if (card.codeUrl2) {
+      window.open(card.codeUrl2, '_blank');
+    }
+  }
+  viewCode3(card: any) {
+    if (card.codeUrl3) {
+      window.open(card.codeUrl3, '_blank');
+    }
+  }
+
   
 }
